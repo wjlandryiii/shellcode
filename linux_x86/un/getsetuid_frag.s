112dffa520b73e9a4cda9b881ca41cb0a259e96a
wjlandryiii <wjlandryiii@gmail.com>
2014-10-12 08:41:21 -0700

    fixed getsetuid

--- linux_x86/un/getsetuid_frag.s
+++ linux_x86/un/getsetuid_frag.s
@@ -4,6 +4,7 @@
 
 .set	SYS_SETUID,23
 .set	SYS_GETEUID,49
+.set	SYS_SETRESUID,164
 
 .section .text
 .global _start
@@ -13,6 +14,8 @@ _start:
 	mov	$SYS_GETEUID,%eax
 	int	$0x80
 
+	mov	%eax,%edx
+	mov	%eax,%ecx
 	mov	%eax,%ebx
-	mov	$SYS_SETUID,%eax
+	mov	$SYS_SETRESUID,%eax
 	int	$0x80
--- linux_x86/un/getsetuid_nz_frag.s
+++ linux_x86/un/getsetuid_nz_frag.s
@@ -4,6 +4,7 @@
 
 .set	SYS_SETUID,23
 .set	SYS_GETEUID,49
+.set	SYS_SETRESUID,164
 
 .section .text
 .global _start
@@ -13,7 +14,9 @@ _start:
 	movb	$SYS_GETEUID,%al
 	int	$0x80
 
+	mov	%eax,%edx
+	mov	%eax,%ecx
 	mov	%eax,%ebx
 	xor	%eax,%eax
-	movb	$SYS_SETUID,%al
+	movb	$SYS_SETRESUID,%al
 	int	$0x80
